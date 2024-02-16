660dcbdcf14e95ddf987806f3fa3b183ce175c8b
Lucas Blomhäll <lucas.blomhall@gmail.com>
2024-02-16 13:43:55 +0100

    made first Path Operator in FastAPI

--- fastapi_folder/main.py
+++ fastapi_folder/main.py
@@ -9,7 +9,7 @@ import uvicorn
 SQLALCHEMY_DATABASE_CONNECTION_STRING = "postgresql://postgres:Vanligt123!@localhost/"
 
 engine = create_engine(SQLALCHEMY_DATABASE_CONNECTION_STRING)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 base = declarative_base()
 base.metadata.create_all(bind=engine)
@@ -27,3 +27,78 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Each field is a column in your DB
+class HisotricalTableGoogl(base):
+    __tablename__ = "googl_historical"
+    date = Column(DATE, primary_key=True)
+    open = Column(DOUBLE_PRECISION)
+    high = Column(DOUBLE_PRECISION)
+    low = Column(DOUBLE_PRECISION)
+    close = Column(DOUBLE_PRECISION)
+    Colume = Column(DOUBLE_PRECISION)
+    dividends = Column(DOUBLE_PRECISION)
+    stocksplits = Column(DOUBLE_PRECISION)
+
+
+# Each field is a column in your DB
+class HisotricalTableMsft(base):
+    __tablename__ = "msft_historical"
+    date = Column(DATE, primary_key=True)
+    open = Column(DOUBLE_PRECISION)
+    high = Column(DOUBLE_PRECISION)
+    low = Column(DOUBLE_PRECISION)
+    close = Column(DOUBLE_PRECISION)
+    Colume = Column(DOUBLE_PRECISION)
+    dividends = Column(DOUBLE_PRECISION)
+    stocksplits = Column(DOUBLE_PRECISION)
+
+
+def get_recent(db, table):
+    most_recent_record = db.query(table).order_by(table.date.desc()).first()
+    return most_recent_record
+
+
+def get_oldest(db, talbe):
+    oldest_record = db.query(table).order_by(table.date).first()
+    return oldest_record
+
+
+class TicketReqest(BaseModel):
+    ticker: str
+    query: str
+
+
+class TicketResponse(BaseModel):
+    ticker: str
+    close_value: float
+
+
+@app.post("/ticker/record")
+async def get_ticker_record(ticker_request: TicketReqest, db=Depends(get_db)):
+    valid_tickers = {
+        'googl': HisotricalTableGoogl,
+        'msft': HisotricalTableMsft,
+    }
+
+    valid_queries = ['recent', 'oldest']
+
+    if ticker_request.ticker not in valid_tickers:
+        raise HTTPException(status_code=400)
+
+    if ticker_request.query not in valid_queries:
+        raise HTTPException(status_code=400)
+
+    table = valid_tickers[ticker_request.ticker]
+
+    if ticker_request.query == 'recent':
+        record = get_recent(db, table)
